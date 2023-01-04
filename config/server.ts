bb952e71b6b7aec586caed00b3b3c30480fecf49
Gameznut <emmanuelapabiekun@gmail.com>
2023-01-04 07:20:37 +0100

    working

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,7 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array(
-      "APP_KEYS",
-      "Q/iLjARKSiR2/48njYpZ0A==",
-      "6MRO7gWwdfvj2PqQIlpynA=="
-    ),
+    keys: env.array('APP_KEYS'),
   },
 });
